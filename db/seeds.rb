6ce84839c2d6811ce32050bc5e8f9d5aed11a3f7
Taras Ivanyuk <ivanyuk.taras@gmail.com>
2021-08-30 17:00:49 +0300

    Seed db with fake daily_menus history

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+drinks = ["Coke", "Juice", "Tea", "Coffe", "Water"]
+
+99.times do |day|
+  # imitate history of created menues on working days only
+  time = 2.month.ago + day.day
+  if time.on_weekday? && !(time > Time.now)
+    menu = DailyMenu.create(created_at: time)
+    3.times do 
+      name = Faker::Food.dish
+      price = Faker::Commerce.price(range: 5..10)
+      menu.food_items.create(name: name, price: price, course: 0)
+      name = Faker::Food.dish
+      price = Faker::Commerce.price(range: 7..10)
+      menu.food_items.create(name: name, price: price, course: 1)
+      name = drinks.sample
+      price = Faker::Commerce.price(range: 2..5)
+      menu.food_items.create(name: name, price: price, course: 2)
+    end
+  end
+end
