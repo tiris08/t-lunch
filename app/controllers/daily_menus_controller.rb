b6a5c3667824ed538e00a99a57504b93e690aee8
Taras Ivanyuk <ivanyuk.taras@gmail.com>
2021-08-31 14:24:10 +0300

    Add show action, view and its route, to output daily-menu history

--- app/controllers/daily_menus_controller.rb
+++ app/controllers/daily_menus_controller.rb
@@ -2,4 +2,9 @@ class DailyMenusController < ApplicationController
   def index
     @daily_menus = DailyMenu.last(10)
   end
+
+  def show
+    @daily_menu = DailyMenu.find(params[:id])
+    @daily_menu_order = DailyMenu.includes(:users)
+  end
 end
