6de0b5695a0bc8d0e87cab5442a4dc76a23b4d9d
Taras Ivanyuk <ivanyuk.taras@gmail.com>
2021-09-02 13:08:23 +0300

    Rename user_order variable to user_order_items

--- app/controllers/daily_menus_controller.rb
+++ app/controllers/daily_menus_controller.rb
@@ -5,7 +5,7 @@ class DailyMenusController < ApplicationController
 
   def show
     @daily_menu = DailyMenu.find(params[:id])
-    @user_order = @daily_menu.food_items.joins(:orders).where(order_items: { user: current_user })
-    @user_order_cost = @user_order.pluck(:price).sum
+    @user_order_items = @daily_menu.food_items.joins(:orders).where(order_items: { user: current_user })
+    @user_order_cost = @user_order_items.pluck(:price).sum
   end
 end
