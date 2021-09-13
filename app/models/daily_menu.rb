96c9febddb38121b3d2c919b09c3b75e1cc76cf4
Taras Ivanyuk <ivanyuk.taras@gmail.com>
2021-09-13 17:11:47 +0300

    add neccessary associations

--- app/models/daily_menu.rb
+++ app/models/daily_menu.rb
@@ -1,4 +1,5 @@
 class DailyMenu < ApplicationRecord
   has_many :food_items, inverse_of: :daily_menu, dependent: :destroy
+  has_many :orders
   accepts_nested_attributes_for :food_items, allow_destroy: true, reject_if: :all_blank
 end
