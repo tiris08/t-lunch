9e2c2c1891b6515f6b62c822e5ab10f21b777121
Taras Ivanyuk <ivanyuk.taras@gmail.com>
2021-09-13 09:41:56 +0300

    add dependent-destroy for food_item order_item

--- app/models/food_item.rb
+++ app/models/food_item.rb
@@ -1,7 +1,7 @@
 class FoodItem < ApplicationRecord
   enum course: [:first_course, :main_course, :drink]
   belongs_to :daily_menu
-  has_many :orders,  class_name: 'OrderItem', foreign_key: 'food_item_id'
+  has_many :orders,  class_name: 'OrderItem', foreign_key: 'food_item_id', dependent: :destroy
   validates :name, presence: true
   validates :price, presence: true
   validates :course, presence: true
