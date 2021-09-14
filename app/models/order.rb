c293fa031fdf22b3c92eae4bfafa94ebd4930619
Taras Ivanyuk <ivanyuk.taras@gmail.com>
2021-09-14 17:13:16 +0300

    refactor models associations to suit new order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,8 @@
 class Order < ApplicationRecord
+  has_many :order_items, inverse_of: :order
+  accepts_nested_attributes_for :order_items, allow_destroy: true, reject_if: :all_blank
+  has_many :food_items, through: :order_items
   belongs_to :user
   belongs_to :daily_menu
-  has_many :order_items
-  has_many :food_items, through: :order_items
+  
 end
